754cf9e36148fe9dabec3a1078e59fdbb65e9c5b
yoyo837 <yoyo837@hotmail.com>
2017-09-14 14:45:23 +0800

    page

---
+++ _page/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+import utils from '../../components/utils'
+export default {
+  name: 'page',
+  mounted() {
+    const query = this.$route.query || {}
+    const id = query['id']
+    delete query.id
+    this.$router.replace(`/page/${id}?${utils.serialize(query)}`)
+  }
+}
+</script>
+
+<style>
+
+</style>
+
