3215759838bd58f720fdac52232a7be81d0e6600
yoyo837 <yoyo837@hotmail.com>
2017-09-15 18:51:08 +0800

    完善票务列表

--- ticket/list.vue
+++ ticket/list.vue
@@ -1,32 +1,108 @@
-<template>
-  <section class="container">
-    <div>
-      <div>选择场馆</div>
-      <ul>
-        <li>123</li>
-      </ul>
-    </div>
-    <div>
-      <div>选择场次时间</div>
-      <ul>
-        <li>123</li>
-      </ul>
-    </div>
-    <div>
-      <div>选择价格</div>
-      <ul>
-        <li>123</li>
-      </ul>
-    </div>
-  </section>
+<template lang="pug">
+  section.container
+    .gap
+    .sec
+      .sec-title 选择场馆
+      ul.sec-content
+        li
+          el-button(type="primary") 场馆1
+    .sec
+      .sec-title 选择场次时间
+      ul.sec-content
+        li
+          el-button(type="primary") 场馆1
+    .sec
+      .sec-title 选择价格
+      ul.sec-content
+        li
+          el-button(type="primary") 场馆1
+    .sec
+      .sec-content.num-content
+        | 数量
+        el-input-number(v-model="num", :min="1", :max="10")
+        | (剩余
+        span 6
+        | 张)
+
+    .fixed-bt
+      el-row
+        el-col(:span="12", class="money") 共计0元
+        el-col(:span="12", class="btn") 去支付
 </template>
 
 <script>
-export default {
+import Vue from 'vue'
+import { Row, Col, Button, InputNumber } from 'element-ui'
+import bdStyleMixin, { DefaultConfig } from '../vue-features/mixins/body-style'
 
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.component(Button.name, Button)
+Vue.component(InputNumber.name, InputNumber)
+
+export default {
+  mixins: [bdStyleMixin],
+  data() {
+    return {
+      bodyClass: `${DefaultConfig.bodyClass} bd-pt-ticket-list`,
+      num: 1
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+body.bd-pt-ticket-list {
+  padding-bottom: 50px;
+}
+</style>
 
+
+<style lang="scss" scoped>
+$padding: 10px;
+.container {
+  .gap {
+    height: 15px;
+    background-color: white;
+  }
+  .sec {
+    .sec-title {
+      color: #777;
+      font-size: 14px;
+      height: 26px;
+      line-height: 26px;
+      padding: 0 $padding;
+    }
+    .sec-content {
+      margin: 0;
+      padding: $padding;
+      list-style-type: none;
+      background-color: white;
+    }
+    .num-content {
+      border-top: 1px solid #eee;
+      .el-input-number {
+        margin: 0 5px;
+      }
+      .el-input-number+span {
+        color: #FF5E20;
+      }
+    }
+  }
+  .fixed-bt {
+    background-color: white;
+    padding: 0;
+    .el-row {
+      .el-col {
+        padding: 15px 0;
+        color: #FF5E20;
+        font-size: 14px;
+      }
+      .el-col.btn {
+        background-color: #FF5E20;
+        color: white;
+      }
+    }
+  }
+}
 </style>
