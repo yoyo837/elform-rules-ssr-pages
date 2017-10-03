807e096c2e7dd22cb35def5621abe2d4fc04fcbe
yoyo837 <yoyo837@hotmail.com>
2017-10-03 22:23:58 +0800

    pageConfig

---
+++ page/500001.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    500001
+  </div>
+</template>
+
+<script>
+import pageConfig from '../vue-features/page-config'
+export default {
+  mounted() {
+    pageConfig.getPageData([{
+      mapId: 102103
+    }, {
+      mapId: 101985
+    }, {
+      mapId: 101986
+    }])
+  }
+}
+</script>
