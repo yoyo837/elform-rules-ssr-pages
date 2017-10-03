01923288f4e829988c32b88afb0a129ca099ceea
yoyo837 <yoyo837@hotmail.com>
2017-10-03 23:46:39 +0800

    500001

--- page/500001.vue
+++ page/500001.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    500001
-  </div>
+  <section class="container">
+    <div class="page-title">
+      <img :src="`${CDN_STATIC_HOST}/themes/mobile/red/red1/images/icon/icong-15.png`"> 我要订场
+      <span>免费开发日历</span>
+    </div>
+    <div class="introduction">
+      {{123}}
+    </div>
+  </section>
 </template>
 
 <script>
@@ -14,7 +20,37 @@ export default {
       mapId: 101985
     }, {
       mapId: 101986
-    }])
+    }]).then(data => {
+      console.log(data)
+    })
+  },
+  data() {
+    return {
+      serverData: {}
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  .page-title {
+    background-color: #f6f6f6;
+    color: #FF5E20;
+    font-size: 15px;
+    padding: 8px;
+    img {
+      width: 18px;
+      height: auto;
+      vertical-align: bottom;
+    }
+    span {
+      float: right;
+      padding: 2px;
+      font-size: 12px;
+      border: 1px solid #FF5E20;
+      border-radius: 15px;
+    }
+  }
+}
+</style>
