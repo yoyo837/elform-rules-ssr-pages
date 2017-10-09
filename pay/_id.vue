788d7a502f9cfa6f39d0ae2f90458d530d9d95f5
yoyo837 <yoyo837@hotmail.com>
2017-10-09 21:26:26 +0800

    appId

--- pay/_id.vue
+++ pay/_id.vue
@@ -320,7 +320,7 @@ export default {
                     }).then(data => {
                       data = data || {}
                       wx.chooseWXPay({
-                        // appId: data.appId,
+                        appId: data.appId,
                         timestamp: data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                         nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
                         package: data.pkg, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
