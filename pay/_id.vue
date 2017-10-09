17890c91970a65aa12114a3e6b5b07f6d09cb526
yoyo837 <yoyo837@hotmail.com>
2017-10-09 20:33:33 +0800

    遗漏appid

--- pay/_id.vue
+++ pay/_id.vue
@@ -310,6 +310,7 @@ export default {
                 }).then(data => {
                   data = data || {}
                   wx.chooseWXPay({
+                    appId: data.appId,
                     timestamp: data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                     nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
                     package: data.pkg, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
@@ -317,7 +318,7 @@ export default {
                     paySign: data.paySign, // 支付签名
                     success: function(res) {
                       // 支付成功后的回调函数
-                      alert('支付成功')
+                      this.$router.push(`/pay/result/${this.dealId}`)
                     }
                   })
                 })
