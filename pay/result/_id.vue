e5def82ea86e44a6ecb22d60dc86615756787d87
yoyo837 <yoyo837@hotmail.com>
2017-10-10 19:27:54 +0800

    paid

--- pay/result/_id.vue
+++ pay/result/_id.vue
@@ -18,7 +18,6 @@ export default {
     this.$http.get('/pay/isPay.do', {
       dealId: this.dealId
     }).then(paid => {
-      alert(paid)
       if (paid) {
         popup.alert('支付成功，正在跳转', {
           callback: () => {
