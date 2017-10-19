b0a162c699d7aeaf41948d344746b62b702559b8
yoyo837 <yoyo837@hotmail.com>
2017-10-19 19:48:57 +0800

    活动列表临时页面UI刷新

--- event/index.vue
+++ event/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <section>
-    活动列表
+  <section class="container container-pd">
+    <Card title-text="我的活动" title-icon="fa fa-id-card">
+      <div class="text-center empty-events">
+        暂无活动
+      </div>
+    </Card>
   </section>
 </template>
 
 <script>
+import bdStyleMixin from '../vue-features/mixins/body-style'
+import Card from '../vue-features/components/Card'
 export default {
-
+  mixins: [bdStyleMixin],
+  components: {
+    Card
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.empty-events {
+  padding: 15px 0;
+  font-size: 12px;
+  color: #999;
+}
 </style>
+
