2bb863fa400bdd612ffdc34161d3f5d0e34df079
yoyo837 <yoyo837@hotmail.com>
2017-10-25 12:52:39 +0800

    修改测试

--- test/upload.vue
+++ test/upload.vue
@@ -1,12 +1,18 @@
 <template>
-  <section class="container container-shrink container-portable">
-    <el-form ref="form" :rules="rules" :model="form">
-      <el-form-item>
+  <section class="container container-pd container-portable">
+    <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+      <el-form-item label="选择文件">
         <el-input type="file" ref="fileComp"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item label="是否私有">
         <el-radio class="radio" v-model="form.privated" :label="true">private</el-radio>
         <el-radio class="radio" v-model="form.privated" :label="false">public</el-radio>
+      </el-form-item>
+      <el-form-item label="选择文件类型" prop="fileType">
+        <el-select v-model="form.fileType" placeholder="请选择">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+          </el-option>
+        </el-select>
       </el-form-item>
     </el-form>
 
@@ -20,7 +26,7 @@
 import Vue from 'vue'
 import popup from '../../components/popup'
 import bdStyleMixin from '../vue-features/mixins/body-style'
-import { Form, FormItem, Button, Row, Col, Input, Radio } from 'element-ui'
+import { Form, FormItem, Button, Row, Col, Input, Radio, Select, Option } from 'element-ui'
 
 Vue.component(Form.name, Form)
 Vue.component(FormItem.name, FormItem)
@@ -29,6 +35,8 @@ Vue.component(Radio.name, Radio)
 Vue.component(Row.name, Row)
 Vue.component(Col.name, Col)
 Vue.component(Input.name, Input)
+Vue.component(Select.name, Select)
+Vue.component(Option.name, Option)
 
 export default {
   mixins: [bdStyleMixin],
@@ -57,8 +65,28 @@ export default {
   },
   data() {
     return {
-      rules: {},
+      options: [
+        {
+          label: '图片',
+          value: 1
+        },
+        {
+          label: '文档',
+          value: 2
+        }
+      ],
+      rules: {
+        fileType: [
+          {
+            type: 'number',
+            required: true,
+            message: '请选择文件类型',
+            trigger: 'blur'
+          }
+        ]
+      },
       form: {
+        fileType: null,
         file: null,
         privated: true
       }
