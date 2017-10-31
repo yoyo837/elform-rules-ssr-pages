e7e3802b77f217471bebe5be0a2862d2e87466a5
yoyo837 <yoyo837@hotmail.com>
2017-10-31 20:39:25 +0800

    整理微信分享代码

--- team/profile.vue
+++ team/profile.vue
@@ -119,7 +119,6 @@ import ProfilePanel from '../vue-features/components/ProfilePanel'
 import Card from '../vue-features/components/Card'
 import bdStyleMixin from '../vue-features/mixins/body-style'
 import { Row, Col, Button } from 'element-ui'
-import wxShare from '../../components/wx-share'
 
 Vue.component(Row.name, Row)
 Vue.component(Col.name, Col)
@@ -200,7 +199,7 @@ export default {
     },
     toShareGuide() {
       if (
-        wxShare.call(this, {
+        this.$wxShare({
           title: `团队:${this.serverData.teamInfo.teamName || ''}`,
           link: `/team/share2join/${this.serverData.teamInfo.id}`,
           img: `${this.CDN_STATIC_HOST}/themes/mobile/blue/images/xicon_${this.serverData.teamInfo.professionalId}.png`,
